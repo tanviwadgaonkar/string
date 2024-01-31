1659c99f3db09018a2aa66c92c60a687da02bdd5
tanviwadgaonkar <156992817+tanviwadgaonkar@users.noreply.github.com>
2024-01-31 20:49:54 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+def animal_cracker():
+    i=input("enter the value of i")
+    j=input("enter the value of j")
+    k=i+j
+    if (i[0]==j[0]):
+        return True
+    else:
+        return False
+
+
+# In[7]:
+
+
+animal_cracker()
+
+
+# In[ ]:
+
+
+
+
